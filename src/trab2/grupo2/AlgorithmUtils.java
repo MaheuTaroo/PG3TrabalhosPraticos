b5a80e5b33f021f5b0f574cbc2091d21027b9324
diogomhc <diogomhc@gmail.com>
2022-11-24 18:21:30 +0000

    Grupo 2

--- src/trab2/grupo2/AlgorithmUtils.java
+++ src/trab2/grupo2/AlgorithmUtils.java
@@ -12,8 +12,27 @@ public class AlgorithmUtils {
             if (compareValue.compare(array[i], array[i + 1]) < 0) ascending = true;
             if (compareValue.compare(array[i], array[i + 1]) > 0) descending = true;
             if (ascending && descending) return false;
-        }
 
+        if(seq.isEmpty()) return false;
+        if(seq.size() == 1) return true;
+        Iterator<E> iter = seq.iterator();
+        E e = iter.next(), tmp = iter.next();
+        boolean ascending = false, descending = false;
+
+        while(iter.hasNext()) {
+            if(compareValue.compare(e, tmp) < 0) ascending = true;
+            else if(compareValue.compare(e, tmp) > 0) descending = true;
+            if(ascending && descending) return false;
+            e = tmp;
+            tmp = iter.next();
+
+            if(!iter.hasNext()) {
+                if(compareValue.compare(e, tmp) < 0) ascending = true;
+                else if(compareValue.compare(e, tmp) > 0) descending = true;
+                if(ascending && descending) return false;
+                break;
+            }
+        }
         return true;
     }
 
@@ -39,6 +58,75 @@ public class AlgorithmUtils {
 
             tmp.add(array[i]);
             if (i == array.length - 2) tmp.add(array[i + 1]);
+
+        if(n < 1) throw new IllegalArgumentException();
+        if(seq.isEmpty()) return List.of();
+        if(seq.size() == 1 && n == 1) return new ArrayList<>(seq);
+        Iterator<E> iter = seq.iterator();
+        E e = iter.next(), temp = iter.next();
+        int segs = 1;
+        ArrayList<E> tmp = new ArrayList<>();
+
+
+        if(seq.size() == 2) {
+            if(cmp.compare(e,temp) < 0) {
+                if(segs == n) {
+                    tmp.add(e);
+                    tmp.add(temp);
+                } else {
+                    segs++;
+                    tmp.add(temp);
+                }
+            } else if(cmp.compare(e, temp) > 0) {
+                if(n == 1) {
+                    segs++;
+                    tmp.add(e);
+                }
+                else if(n == 2) {
+                    segs++;
+                    tmp.add(temp);
+                }
+            }
+        }
+
+        while(iter.hasNext()) {
+            if(cmp.compare(e, temp) > 0) {
+                if(segs == n) {
+                    tmp.add(e);
+                    break;
+                }
+                tmp = new ArrayList<>();
+                segs++;
+                e = temp;
+                temp = iter.next();
+                if(!iter.hasNext()) {
+                    if(cmp.compare(e, temp) < 0) {
+                        tmp.add(e);
+                        tmp.add(temp);
+                        break;
+                    }
+                }
+                continue;
+            }
+
+            tmp.add(e);
+            e = temp;
+            temp = iter.next();
+            if(!iter.hasNext()) {
+                if(cmp.compare(e, temp) < 0) {
+                    tmp.add(e);
+                    tmp.add(temp);
+                    break;
+                }
+                if(segs + 1 == n && cmp.compare(e,temp) > 0) {
+                    segs++;
+                    tmp = new ArrayList<>();
+                    tmp.add(temp);
+                    break;
+                }
+                tmp.add(e);
+                break;
+            }
         }
 
         if (subSeqs < n) return List.of();
@@ -49,6 +137,15 @@ public class AlgorithmUtils {
         List<Integer> l = List.of(1,2,3,2,4,5,3,6,3);
 
         System.out.println(getSubSequences(l, 4, Integer::compare));
+    public static <K,V,C extends Collection<V>> void addAll(BufferedReader in, Map<K,C> m, Function<String, V> getValue, Function<V, K> getKey, Supplier<C> supC)  throws IOException {
+        String s;
+        while((s = in.readLine()) != null) {
+            V v = getValue.apply(s);
+            K k = getKey.apply(v);
+        }
     }
 
+    public static <K,V,C extends Collection<V>> void forEachIf(Map<K,C> m, Predicate<K> pred, Consumer<V> action) {
+
+    }
 }

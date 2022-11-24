333d436c79c95f414a3ae8f9547d2f84c7c1811f
diogomhc <114083369+diogomhc@users.noreply.github.com>
2022-11-24 18:24:46 +0000

    Update AlgorithmUtils.java

--- src/trab2/grupo2/AlgorithmUtils.java
+++ src/trab2/grupo2/AlgorithmUtils.java
@@ -5,15 +5,6 @@ import java.util.*;
 public class AlgorithmUtils {
     public static <E> boolean isOrdered(Collection<E> seq, Comparator<E> compareValue ) {
         if (seq.isEmpty()) return false;
-
-        boolean ascending = false, descending = false;
-        E[] array = (E[])seq.toArray();
-        for (int i = 0; i < array.length - 1; i++) {
-            if (compareValue.compare(array[i], array[i + 1]) < 0) ascending = true;
-            if (compareValue.compare(array[i], array[i + 1]) > 0) descending = true;
-            if (ascending && descending) return false;
-
-        if(seq.isEmpty()) return false;
         if(seq.size() == 1) return true;
         Iterator<E> iter = seq.iterator();
         E e = iter.next(), tmp = iter.next();
@@ -39,28 +30,6 @@ public class AlgorithmUtils {
     public static <E> List<E> getSubSequences(Collection<E> seq, int n, Comparator<E> cmp ) {
         if (n < 1) throw new IllegalArgumentException();
         if (seq.isEmpty()) return List.of();
-
-        int subSeqs = 1;
-
-        E[] array = (E[])seq.toArray();
-        ArrayList<E> tmp = new ArrayList<>();
-
-        for (int i = 0; i < array.length - 1; i++) {
-            if (cmp.compare(array[i], array[i + 1]) > 0) {
-                if (subSeqs == n) {
-                    tmp.add(array[i]);
-                    break;
-                }
-                tmp = new ArrayList<>();
-                subSeqs++;
-                continue;
-            }
-
-            tmp.add(array[i]);
-            if (i == array.length - 2) tmp.add(array[i + 1]);
-
-        if(n < 1) throw new IllegalArgumentException();
-        if(seq.isEmpty()) return List.of();
         if(seq.size() == 1 && n == 1) return new ArrayList<>(seq);
         Iterator<E> iter = seq.iterator();
         E e = iter.next(), temp = iter.next();
@@ -129,23 +98,7 @@ public class AlgorithmUtils {
             }
         }
 
-        if (subSeqs < n) return List.of();
+        if (segs < n) return List.of();
         return tmp;
-    }
-
-    public static void main(String[] args) {
-        List<Integer> l = List.of(1,2,3,2,4,5,3,6,3);
-
-        System.out.println(getSubSequences(l, 4, Integer::compare));
-    public static <K,V,C extends Collection<V>> void addAll(BufferedReader in, Map<K,C> m, Function<String, V> getValue, Function<V, K> getKey, Supplier<C> supC)  throws IOException {
-        String s;
-        while((s = in.readLine()) != null) {
-            V v = getValue.apply(s);
-            K k = getKey.apply(v);
-        }
-    }
-
-    public static <K,V,C extends Collection<V>> void forEachIf(Map<K,C> m, Predicate<K> pred, Consumer<V> action) {
-
     }
 }

dad548da8a728f78c4d28c1f5c9b0856ffd44bd7
diogomhc <diogomhc@gmail.com>
2022-11-28 14:54:13 +0000

    Update AlgorithmUtils.java

--- src/trab2/grupo2/AlgorithmUtils.java
+++ src/trab2/grupo2/AlgorithmUtils.java
@@ -1,5 +1,7 @@
 package trab2.grupo2;
 
+import java.util.function.*;
+import java.io.*;
 import java.util.*;
 
 public class AlgorithmUtils {
@@ -100,5 +102,33 @@ public class AlgorithmUtils {
 
         if (segs < n) return List.of();
         return tmp;
+    }
+
+    <K,V,C extends Collection<V>> void addAll(BufferedReader in, Map<K,C> m, Function<String, V> getValue, Function<V, K> getKey, Supplier<C> supC) throws IOException {
+        String s;
+        while((s = in.readLine()) != null) {
+            V value = getValue.apply(s);
+            K key = getKey.apply(value);
+            if(!m.containsKey(key)) {
+                m.put(key, supC.get());
+            }
+            m.get(key).add(value);
+        }
+    }
+
+    <K,V,C extends Collection<V>> void forEachIf( Map<K,C> m, Predicate<K> pred, Consumer<V> action) {
+        m.forEach((key, c) -> {
+            if(pred.test(key))
+                for (V v : m.get(key))
+                    action.accept(v);
+        });
+    }
+
+    <K,V,C extends Collection<V>> void forEachIf( Map<K,C> m, BiPredicate<K,C> p, BiConsumer<K,C> action) {
+        m.forEach((key, c) -> {
+            if(p.test(key, c))
+                for(V v : m.get(key))
+                    action.accept(key, c);
+        });
     }
 }

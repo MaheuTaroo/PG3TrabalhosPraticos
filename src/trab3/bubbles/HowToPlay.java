2893ff24c3bd2ca27d7e7c275883c90ef1bec63e
MaheuTaroo <pantupa@gmail.com>
2023-01-05 21:29:14 +0000

    Tutorial e começo de estatísticas

--- src/trab3/bubbles/HowToPlay.java
+++ src/trab3/bubbles/HowToPlay.java
@@ -3,23 +3,50 @@ package trab3.bubbles;
 import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import javax.swing.*;
 
 public class HowToPlay extends JDialog {
     public HowToPlay() {
-        // TODO - acabar ficheiro HowToPlay.txt e formular janela
+        // TODO - acabar ficheiro bubbles.txt e formular janela
 
         super();
+        setTitle("How to play");
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        try (BufferedReader br = new BufferedReader(new FileReader("D:\\isel\\pg3\\Projetos\\PG3TrabalhosPraticos\\src\\trab3\\HowToPlay.txt"))) {
-            String[] tips = br.lines().toArray(String[]::new);
-            int index = 0;
+        setSize(500, 500);
+        setResizable(false);
 
+        try (BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\bubbles.txt"))) {
+            String[] tips = br.lines().toArray(String[]::new);
+
+            JTextArea howToPlay = new JTextArea();
+            //howToPlay.read(br, null);
+
+            JScrollPane scroll = new JScrollPane(howToPlay);
+            scroll.setVerticalScrollBar(scroll.createVerticalScrollBar());
+            scroll.setHorizontalScrollBar(scroll.createHorizontalScrollBar());
+
+            howToPlay.setEditable(false);
+
+            for (int i = 0; i < tips.length; i++) {
+                howToPlay.append(tips[i]);
+                if (i != tips.length - 1) howToPlay.append("\n");
+            }
+
+            JButton close = new JButton();
+            close.addActionListener(al -> dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING)));
+
+            Container c = getContentPane();
+
+            c.add(scroll, BorderLayout.CENTER);
+            c.add(close, BorderLayout.SOUTH);
+
+            setVisible(true);
         }
         catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "There was an error fetching the tutorial: " + e.getLocalizedMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "There was an error opening the \"how to play\" window: " + e.getLocalizedMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
         }
     }
--- src/trab3/bubbles/Players.java
+++ src/trab3/bubbles/Players.java
@@ -1,6 +1,33 @@
 package trab3.bubbles;
 
 public class Players {
+    String playerName;
+    int gamesPlayed, gamesWon,
+        bestScore, avgScore,
+        bestTime, avgTime,
+        minBubbles, avgBubbles, maxBubbles;
 
+    public Players(String name, int playedGames, int victories,
+                                int bestPoints, int averagePoints,
+                                int quickestTime, int averageTime,
+                                int leastBubbles, int averageBubbles, int mostBubbles) {
+        playerName = name;
+        gamesPlayed = playedGames;
+        gamesWon = victories;
+        bestScore = bestPoints;
+        avgScore = averagePoints;
+        bestTime = quickestTime;
+        avgTime = averageTime;
+        minBubbles = leastBubbles;
+        avgBubbles = averageBubbles;
+        maxBubbles = mostBubbles;
+    }
 
+    public Players(String name) {
+        this(name, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+
+    public boolean equals(Object o) {
+        return o instanceof Players && playerName.equals(((Players) o).playerName);
+    }
 }
